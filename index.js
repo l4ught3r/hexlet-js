3bab69cee1c9c30594e6eadc03336d1783f2de96
Anton Kuchin <l4ught3r@mail.ru>
2022-07-29 14:03:27 +0700

    lesson 7

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 import _ from "lodash";
-console.log('Hello, Hexlet!')
-console.log(_.last(['one', 'two']));
+console.log("Hello, Hexlet!");
+console.log(_.last(["one", "two"]));
+
+let person = {
+  name: "Yoda",
+  designation: "Jedi Master ",
+};
+
+function trainJedi(jediWarrion) {
+  if (jediWarrion.name === "Yoda") {
+    console.log("No need! already trained");
+  }
+  console.log(`Training ${jediWarrion.name} complete`);
+}
+
+trainJedi(person);
+trainJedi({ name: "Adeel", designation: "padawan" });
